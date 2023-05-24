14c42faee8eba37c8bece5099e6ac87784d2db41
zx <67887489+tan-zx@users.noreply.github.com>
2023-05-24 16:15:50 +0800

    feat: rename to postman and create more resources

--- index.ts
+++ index.ts
@@ -1,13 +1,8 @@
 import {
-  Bastion,
-  CloudWatchToDatadogLogs,
-  DatadogIntegration,
   Ecs,
   EcsWorker,
-  GithubOidc,
   loadAwsProviderDefaultTags,
   MemoryDbRedisCluster,
-  Rds,
   SecurityGroupConnection,
   Vpc,
 } from '@opengovsg/pulumi-components'
@@ -15,24 +10,24 @@ import * as aws from '@pulumi/aws'
 import * as pulumi from '@pulumi/pulumi'
 
 const config = new pulumi.Config()
-// const ecsConfig = config.getObject('ecs') as Partial<{
-//   server?: {
-//     minCapacity: number
-//     maxCapacity: number
-//   }
-//   worker?: {
-//     minCapacity: number
-//     maxCapacity: number
-//   }
-// }>
-// const memoryDbConfig = config.getObject('memorydb') as {
-//   nodeType?: string
-// }
+const ecsConfig = config.getObject('ecs') as Partial<{
+  server?: {
+    minCapacity: number
+    maxCapacity: number
+  }
+  worker?: {
+    minCapacity: number
+    maxCapacity: number
+  }
+}>
+const memoryDbConfig = config.getObject('memorydb') as {
+  nodeType?: string
+}
 
 export const { env, project, team } = loadAwsProviderDefaultTags()
 
 // Having env in every name makes multiple env (e.g. prod + stg) in one AWS account possible
-const name = `postmangovsg-${env}`
+const name = 'postman'
 const isProd = env === 'prod'
 const baseUrl = 'postman.gov.sg'
 export const domainName = isProd ? baseUrl : `${env}.${baseUrl}`
@@ -42,7 +37,6 @@ const vpc = new Vpc(name, {
   isProd,
   secondOctet: 27,
 })
-/*
 // ========================== ECS (including LB) + CF/ACM cert ==========================
 const ecr = new aws.ecr.Repository(
   name,
@@ -62,7 +56,6 @@ const ecs = new Ecs(name, {
   loadBalancingArgs: {
     allowCloudFlareOriginatedTraffic: true,
     allowOgpVpnOriginatedTraffic: true,
-    httpsCertificateArn: acmCertArn,
   },
   scalingArgs: {
     minCapacity: ecsConfig?.server?.minCapacity ?? undefined,
@@ -106,52 +99,31 @@ new aws.cloudwatch.LogGroup(`${name}-worker-dd-logs`, {
 })
 
 // ======================================== RDS =========================================
-const rds = new Rds(name, {
-  dangerouslyPrepareForDeletion: !isProd,
-  includeSsmSecrets: true,
-  isProd,
-  vpc,
-})
-
-export const psqlCommand = rds.psqlCommand
-
 // Set security group for ecs to RDS
-new SecurityGroupConnection(`${name}-ecs-task-to-rds`, {
-  description: 'Allow traffic from ECS Task to RDS',
-  fromSg: ecs.taskSecurityGroup,
-  toSg: rds.securityGroup,
-  port: 5432,
-})
+// new SecurityGroupConnection(`${name}-ecs-task-to-rds`, {
+//   description: 'Allow traffic from ECS Task to RDS',
+//   fromSg: ecs.taskSecurityGroup,
+//   toSg: rds.securityGroup,
+//   port: 5432,
+// })
 
 // Set security group for ecs worker to RDS
-new SecurityGroupConnection(`${name}-ecs-worker-task-to-rds`, {
-  description: 'Allow traffic from ECS Worker Task to RDS',
-  fromSg: worker.securityGroup,
-  toSg: rds.securityGroup,
-  port: 5432,
-})
+// new SecurityGroupConnection(`${name}-ecs-worker-task-to-rds`, {
+//   description: 'Allow traffic from ECS Worker Task to RDS',
+//   fromSg: worker.securityGroup,
+//   toSg: rds.securityGroup,
+//   port: 5432,
+// })
 
 // ======================================= Redis =========================================
 
-const redis = new MemoryDbRedisCluster(
-  `${name}-redis`,
-  {
-    vpc,
-    nodeType: memoryDbConfig.nodeType,
-    clusterArgs: {
-      numReplicasPerShard: 1,
-      aclName: `${name}-memdb-acl`, // This ACL has to manually created
-    },
+const redis = new MemoryDbRedisCluster(`${name}-redis`, {
+  vpc,
+  nodeType: memoryDbConfig.nodeType,
+  clusterArgs: {
+    numReplicasPerShard: 1,
   },
-  {
-    ignoreChanges: [
-      'numShards',
-      'numReplicasPerShard',
-      'aclName',
-      'clusterArgs',
-    ],
-  },
-)
+})
 
 export const redisUrl = redis.clusterAddress
 
@@ -170,48 +142,3 @@ new SecurityGroupConnection(`${name}-ecs-worker-task-to-redis`, {
   toSg: redis.securityGroup,
   port: redis.clusterPort,
 })
-
-// ======================================= Bastion ======================================
-
-const bastionKeyName = `postmangovsg-${env}-bastion-kp`
-const bastion = new Bastion(name, { vpc, keyName: bastionKeyName })
-export const bastionSshCommand = bastion.sshCommand
-
-// Set security group for bastion to RDS
-new SecurityGroupConnection(`${name}-bastion-to-rds`, {
-  description: 'Allow traffic from Bastion EC2 to RDS',
-  fromSg: bastion.securityGroup,
-  toSg: rds.securityGroup,
-  port: 5432,
-})
-
-// Set security group for bastion to Redis
-new SecurityGroupConnection(`${name}-bastion-to-redis`, {
-  description: 'Allow traffic from Bastion EC2 to Redis',
-  fromSg: bastion.securityGroup,
-  toSg: redis.securityGroup,
-  port: redis.clusterPort,
-})
-
-// ====================================== Datadog =======================================
-if (!isProd) {
-  new DatadogIntegration(name, {
-    includedRegions: ['ap-southeast-1'],
-    useShortEnv: false,
-    integrationArgs: {
-      hostTags: [`service:${project}`, `team:${team}`],
-    },
-  })
-}
-
-new CloudWatchToDatadogLogs(`${env}-server`, {
-  logGroupName: serverLogs.name,
-  presetLogFilterPattern: 'JSON',
-  useShortEnv: false,
-})
-new CloudWatchToDatadogLogs(`${env}-worker`, {
-  logGroupName: workerLogs.name,
-  presetLogFilterPattern: 'JSON',
-  useShortEnv: false,
-})
- */
